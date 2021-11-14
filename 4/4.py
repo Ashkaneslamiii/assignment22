66f5733132073e6bb97fb18ce88ee229e16d77e5
Ashkaneslamiii <91659078+Ashkaneslamiii@users.noreply.github.com>
2021-11-14 17:25:04 +0100

    Create 4.py

---
+++ 4/4.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+list = []
+for i in range (15):
+    img = cv2.imread(f"h{i}.jpg",0)
+    list.append(img)
+
+row , col = img.shape
+result = np.zeros((row,col), dtype = "uint8")
+
+for pic in list:
+    result += img//16
+
+cv2.imshow("result", result)
+cv2.waitKey()
+    
